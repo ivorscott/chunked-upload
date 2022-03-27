e94d5d39fad598684e57087116a3f7503c862d4a
Ivor Scott <ivor@devpie.io>
2022-03-28 01:08:30 +0200

    resumable uploads

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module poc-chunked-uploads
 
 go 1.17
+
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/tus/tusd v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,15 @@
 // In reality, we would send each chunk to Azure directly as blocks without saving the file to the filesystem.
 // https://docs.microsoft.com/en-us/rest/api/storageservices/put-block
 // https://docs.microsoft.com/en-us/rest/api/storageservices/put-block-list
+// https://github.com/tus/tusd/blob/fbc83f508ecccbdadc485be5d42fabc565724e41/pkg/handler/doc.go
+// https://github.com/tus/tusd/blob/fbc83f508ecccbdadc485be5d42fabc565724e41/pkg/azurestore/azurestore.go#L38
 package main
 
 import (
 	"fmt"
-	"io"
-	"log"
+	"github.com/tus/tusd/pkg/filestore"
+	tusd "github.com/tus/tusd/pkg/handler"
 	"net/http"
-	"os"
-	"strconv"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,62 +18,53 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-func uploadChunkHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "PUT" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	chunk, fileHeader, err := r.FormFile("file")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	err = os.MkdirAll("./uploads", os.ModePerm)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var file *os.File
-
-	if file == nil {
-		file, err = os.OpenFile(fmt.Sprintf("%s/%s", "./uploads", fileHeader.Filename), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			panic("Error creating file on the filesystem: " + err.Error())
-		}
-	}
-	defer file.Close()
-
-	bytes, err := io.Copy(file, chunk)
-	if err != nil {
-		panic("Error during chunk write:" + err.Error())
-	}
-
-	fmt.Printf("Upload bytes written %d\n", bytes)
-
-	length, _ := strconv.Atoi(r.Header.Get("Upload-Length"))
-	offset, _ := strconv.Atoi(r.Header.Get("Upload-Offset"))
-	maxSize, _ := strconv.Atoi(r.Header.Get("Upload-Max-Chunk-Size"))
-
-	fmt.Println("Upload Offset", offset)
-	fmt.Println("Upload length", length)
-	fmt.Println("Upload Chunks", r.Header.Get("Upload-Chunks"))
-	fmt.Println("Upload Chunk-Size", maxSize)
-
-	if length-offset <= maxSize {
-		fmt.Println("Last Upload Chunk Detected")
-	}
-	fmt.Fprintf(w, "Upload successful")
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/upload", uploadChunkHandler)
-
-	if err := http.ListenAndServe(":4500", mux); err != nil {
-		log.Fatal(err)
+	// Create a new FileStore instance which is responsible for
+	// storing the uploaded file on disk in the specified directory.
+	// This path _must_ exist before tusd will store uploads in it.
+	// If you want to save them on a different medium, for example
+	// a remote FTP server, you can implement your own storage backend
+	// by implementing the tusd.DataStore interface.
+	store := filestore.FileStore{
+		Path: "./uploads",
 	}
+
+	// A storage backend for tusd may consist of multiple different parts which
+	// handle upload creation, locking, termination and so on. The composer is a
+	// place where all those separated pieces are joined together. In this example
+	// we only use the file store but you may plug in multiple.
+	composer := tusd.NewStoreComposer()
+	store.UseIn(composer)
+
+	// Create a new HTTP handler for the tusd server by providing a configuration.
+	// The StoreComposer property must be set to allow the handler to function.
+	handler, err := tusd.NewHandler(tusd.Config{
+		BasePath:              "/files/",
+		StoreComposer:         composer,
+		NotifyCompleteUploads: true,
+	})
+	if err != nil {
+		panic(fmt.Errorf("Unable to create handler: %s", err))
+	}
+
+	// Start another goroutine for receiving events from the handler whenever
+	// an upload is completed. The event will contains details about the upload
+	// itself and the relevant HTTP request.
+	go func() {
+		for {
+			event := <-handler.CompleteUploads
+			fmt.Printf("Upload %s finished\n", event.Upload.ID)
+		}
+	}()
+
+	http.HandleFunc("/", indexHandler)
+	// Right now, nothing has happened since we need to start the HTTP server on
+	// our own. In the end, tusd will start listening on and accept request at
+	// http://localhost:1080/files
+	http.Handle("/files/", http.StripPrefix("/files/", handler))
+	err = http.ListenAndServe(":1080", nil)
+	if err != nil {
+		panic(fmt.Errorf("Unable to listen: %s", err))
+	}
+
 }
